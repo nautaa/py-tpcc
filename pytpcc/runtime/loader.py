3605bb31430acfc626709a20ca4fbd6afd14078b
buduo.jyh <buduo.jyh@alibaba-inc.com>
2025-03-19 17:19:12 +0800

    miniob

--- pytpcc/runtime/loader.py
+++ pytpcc/runtime/loader.py
@@ -225,6 +225,7 @@ class Loader:
 
         c_phone = rand.nstring(constants.PHONE, constants.PHONE)
         c_since = datetime.now()
+        c_since = c_since.strftime(constants.DATE_FORMAT)
         c_credit = constants.BAD_CREDIT if badCredit else constants.GOOD_CREDIT
         c_credit_lim = constants.INITIAL_CREDIT_LIM
         c_discount = rand.fixedPoint(constants.DISCOUNT_DECIMALS, constants.MIN_DISCOUNT, constants.MAX_DISCOUNT)
@@ -252,6 +253,7 @@ class Loader:
     def generateOrder(self, o_w_id, o_d_id, o_id, o_c_id, o_ol_cnt, newOrder):
         """Returns the generated o_ol_cnt value."""
         o_entry_d = datetime.now()
+        o_entry_d = o_entry_d.strftime(constants.DATE_FORMAT)
         o_carrier_id = constants.NULL_CARRIER_ID if newOrder else rand.number(constants.MIN_CARRIER_ID, constants.MAX_CARRIER_ID)
         o_all_local = constants.INITIAL_ALL_LOCAL
         return [ o_id, o_c_id, o_d_id, o_w_id, o_entry_d, o_carrier_id, o_ol_cnt, o_all_local ]
@@ -264,6 +266,7 @@ class Loader:
         ol_i_id = rand.number(1, max_items)
         ol_supply_w_id = ol_w_id
         ol_delivery_d = datetime.now()
+        ol_delivery_d = ol_delivery_d.strftime(constants.DATE_FORMAT)
         ol_quantity = constants.INITIAL_QUANTITY
 
         ## 1% of items are from a remote warehouse
@@ -311,6 +314,7 @@ class Loader:
         h_w_id = h_c_w_id
         h_d_id = h_c_d_id
         h_date = datetime.now()
+        h_date = h_date.strftime(constants.DATE_FORMAT)
         h_amount = constants.INITIAL_AMOUNT
         h_data = rand.astring(constants.MIN_DATA, constants.MAX_DATA)
         return [ h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data ]
